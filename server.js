e984e4edf1c993ab00349f9e82669fd3303c4cb7
Weslley Braga <wesbragagt@gmail.com>
2019-09-13 23:08:20 -0500

    using express

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-var http = require('http');
-var fs = require('fs');
-
-var myReadStream = fs.createReadStream(__dirname + '/read.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')
-
-myReadStream.pipe(myWriteStream);
-
-var server = http.createServer(function(req, res) {
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var myReadStream = fs.createReadStream(__dirname + '/read.txt', 'utf8');
-    myReadStream.pipe(res);
-
-});
-
-server.listen(3000, '127.0.0.1');
-console.log('now listening on port 3000');
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000, console.log('listening...'))
