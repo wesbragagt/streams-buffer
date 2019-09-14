52df5df790e4bb8736d6d1e86bef2395f9fdc3e9
Weslley Braga <wesbragagt@gmail.com>
2019-09-13 23:22:16 -0500

    building a pipe in express

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
+
+
+// var data = [{name: 'john'}, {name:'michael'}]
+
+// for (let index = 0; index < 1000; index++) {
+//     data.push({name: `dle${Math.random()}`})
+    
+// }
+
+// fs.writeFileSync('names.json', JSON.stringify(data), function(err, result){
+//     try{
+//         console.log(result)
+//     } catch(err){
+//         console.log(err)
+//     }
+// })
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    var readStream = fs.createReadStream(__dirname + '/names.json')
+    readStream.pipe(res)
+    readStream.on(row => console.log(row))
 })
  
 app.listen(3000, console.log('listening...'))
